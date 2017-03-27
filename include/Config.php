52016bba7de72b71a07365c885c70e745d793cc5
narendharlive <narendharlive@gmail.com>
2017-03-27 09:44:24 +0530

    'master: rootConfig'

--- include/Config.php
+++ include/Config.php
@@ -3,7 +3,7 @@
  * Database configuration
  */
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'na12345');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'task_manager');
 
